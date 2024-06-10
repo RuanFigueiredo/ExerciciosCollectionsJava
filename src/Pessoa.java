93e29a7d282efd35cf4d0107c4ce955aa54f0b04
RuanFigueiredo <ruanthiago67@gmail.com>
2024-06-10 14:38:11 -0300

    exercicio usando metodo ToString representando os atributos

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,20 @@
 public class Pessoa {
+   String nome;
+   int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "nome:" + nome + " , idade: "+ idade;
+    }
+
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa(" Ruan", 33);
+        System.out.println(pessoa1);
+
+    }
 }

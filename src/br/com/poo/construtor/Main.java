0bdda56b75112d04a63b9839898b3d7ea5e427d2
RuanFigueiredo <ruanthiago67@gmail.com>
2024-06-12 15:13:24 -0300

    listando produtos com arraylist e mostrando produto e o tamanho da lista

---
+++ src/br/com/poo/construtor/Main.java
@@ -0,0 +1,21 @@
+package br.com.poo.construtor;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Produto> listaDeProdutos = new ArrayList<>();
+
+        listaDeProdutos.add(new Produto("Abajur",2.5,200));
+
+        listaDeProdutos.add(new Produto("Janela",100.50,500));
+
+        listaDeProdutos.add(new Produto("Porta",200.5,150));
+
+        System.out.println("Tamanho da lista: " +listaDeProdutos.size());
+        System.out.println("produto 1: " + listaDeProdutos.get(0).getNome());
+
+
+
+    }
+}

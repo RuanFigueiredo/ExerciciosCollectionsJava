c13c2ae31a0f4e3e0b8e8b03793db2eab1dbd3ae
RuanFigueiredo <ruanthiago67@gmail.com>
2024-06-19 15:49:56 -0300

    adicionando itens na lista

---
+++ src/br/com/poo/CastingOrInstanceOf/PrincipalProdutos.java
@@ -0,0 +1,17 @@
+package br.com.poo.CastingOrInstanceOf;
+
+import java.util.ArrayList;
+
+public class PrincipalProdutos {
+    public static void main(String[] args) {
+        ArrayList<Produto> listaProduto = new ArrayList<>();
+
+        listaProduto.add(new Produto("Macarrão", 6.58));
+        listaProduto.add(new Produto("Feijão preto", 10.80));
+
+
+
+
+
+    }
+}

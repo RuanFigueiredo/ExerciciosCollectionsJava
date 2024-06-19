699a8b7ec8a6824b80a755fabaa58576b4989b99
RuanFigueiredo <ruanthiago67@gmail.com>
2024-06-19 15:01:01 -0300

    fazendo uma verificação antes de fazer o casting

--- src/br/com/poo/CastingOrInstanceOf/PrincipalBichos.java
+++ src/br/com/poo/CastingOrInstanceOf/PrincipalBichos.java
@@ -2,7 +2,19 @@ package br.com.poo.CastingOrInstanceOf;
 
 public class PrincipalBichos {
 
-    Cachorro bicho1 = new Cachorro();
+    public static void main(String[] args) {
 
-    Animal animal = (Animal) bicho1;g
+        Cachorro bicho1 = new Cachorro();
+
+        if(bicho1 instanceof Animal){
+            Animal animal = (Animal)bicho1;
+            System.out.println("Casting feito!");
+        }else{
+            System.out.println("O objeto não é do tipo animal!");
+        }
+
+
+    }
+
+
 }

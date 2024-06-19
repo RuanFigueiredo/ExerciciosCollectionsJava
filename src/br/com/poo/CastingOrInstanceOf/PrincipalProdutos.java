4c1f4abc6962f37bf7fd75116c8c7963d6bdd1c8
RuanFigueiredo <ruanthiago67@gmail.com>
2024-06-19 15:55:13 -0300

    calculo da média dos preços  produtos

--- src/br/com/poo/CastingOrInstanceOf/PrincipalProdutos.java
+++ src/br/com/poo/CastingOrInstanceOf/PrincipalProdutos.java
@@ -6,8 +6,20 @@ public class PrincipalProdutos {
     public static void main(String[] args) {
         ArrayList<Produto> listaProduto = new ArrayList<>();
 
-        listaProduto.add(new Produto("Macarrão", 6.58));
-        listaProduto.add(new Produto("Feijão preto", 10.80));
+        listaProduto.add(new Produto("Macarrão", 3.58));
+        listaProduto.add(new Produto("Feijão preto", 8.80));
+        listaProduto.add(new Produto("Feijão macassar", 7.50));
+        listaProduto.add(new Produto("Arroz", 4.80));
+
+        double somaDosPrecos = 0.0;
+
+        for(Produto produto: listaProduto){
+            somaDosPrecos += produto.getPreco();
+        }
+
+        double precoMedio= somaDosPrecos / listaProduto.size();
+
+        System.out.println("O preço médio desses produtos é de: R$ "+ precoMedio);
 
 
 
